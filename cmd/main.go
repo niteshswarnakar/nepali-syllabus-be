0b2bc84d2ba782478a9a9d10f272cd14d5037e16
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2024-11-27 10:40:16 +0545

    initial setup

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/labstack/echo/v4"
+
+func main() {
+	server := echo.New()
+	server.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello World!")
+	})
+
+	server.Logger.Fatal(server.Start(":8080"))
+
+}
